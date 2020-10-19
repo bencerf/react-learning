de18171deefaf5f91f5a016935c26b98b291292a
MiryksV <cerf.benjamin25@gmail.com>
2020-10-19 18:17:54 -0400

    53: change App from basics to a functional App component + format

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,56 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [state, setState] = useState({
     persons: [
       { name: 'Max', age: '28' },
       { name: 'Manu', age: '29' },
       { name: 'Steph', age: '26' }
     ],
     otherState: 'some other value'
+  });
+
+  const switchNameHandler = (newName) => {
+    // console.log('Was clicked !')
+    setState({
+      persons: [
+        { name: newName, age: '28' },
+        { name: 'Manuel', age: '29' },
+        { name: 'Stephanie', age: '26' }
+      ]
+    })
   };
 
-
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked !')
-    // this.state.persons[0].name = 'Maximilian';
-    this.setState( {
+  const nameChangedHandler = (event) => {
+    setState({
       persons: [
-        { name: newName, age:'28' },
-        { name: 'Manuel', age:'29' },
-        { name: 'Stephanie', age:'26' }
+        { name: 'Max', age: '28' },
+        { name: event.target.value, age: '29' },
+        { name: 'Stephanie', age: '26' }
       ]
-    } )
-  }
+    })
+  };
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age:'28' },
-        { name: event.target.value, age:'29' },
-        { name: 'Stephanie', age:'26' }
-      ]
-    } )
-  }
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
 
-  
-  render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-
-    return (
-      <div className="App">
-        {/* <div className="App-header">
+  return (
+    <div className="App">
+      {/* <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
@@ -58,27 +53,32 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
 
-        <h1>Hi, I'm a React App</h1>
-        <p>Thi is really working!</p>
-        <button style={style} onClick={() => this.switchNameHandler('Maximiian !!')} >Switch Name</button>
+      <h1>Hi, I'm a React App</h1>
+      <p>Thi is really working!</p>
+      <button
+        style={style}
+        onClick={() => switchNameHandler('Maximiian !!')}
+      >
+        Switch Name
+      </button>
 
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}/>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max')}
-          changed={this.nameChangedHandler}>
-            My Hobbies: Racing !
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}/>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App !!!'));
-  }
-}
+      <Person
+        name={state.persons[0].name}
+        age={state.persons[0].age} />
+      <Person
+        name={state.persons[1].name}
+        age={state.persons[1].age}
+        click={switchNameHandler.bind(this, 'Max')}
+        changed={nameChangedHandler}
+      >
+        My Hobbies: Racing !
+      </Person>
+      <Person
+        name={state.persons[2].name}
+        age={state.persons[2].age} />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App !!!'));
+};
 
-export default App;
+export default app;
